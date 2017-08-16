7d8cef02a22704654f4c6396800efca814fe0232
Jasmine Zhang <jasminezhang@live.com>
2017-08-16 14:57:52 -0700

    KILL ME

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public Health health;
+
+    // Use this for initialization
+    void Awake ()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            health.ChangeHealth(-1);
+            Destroy(gameObject);
+        }
+    }
+}
